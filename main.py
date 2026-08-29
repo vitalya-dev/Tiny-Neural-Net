c582dca37c999a1d914d6576a47db0e610ba05d1
Vitalya <vitalya.dev@gmail.com>
2026-08-29 09:20:51 +0300

    feat: изменить форму массивов данных для сверточной сети.

--- main.py
+++ main.py
@@ -7,35 +7,25 @@ import struct
 import json
 
 def load_mnist_idx(train_images_path, train_labels_path, test_images_path, test_labels_path):
-    # Вспомогательная функция для чтения одного IDX файла
     def read_idx(filename):
+        import struct
         with open(filename, 'rb') as f:
-            # Читаем первые 4 байта (magic number)
-            # '>HBB' означает: Big-endian, H (2 байта нулей), B (1 байт типа данных), B (1 байт размерности)
             zero, data_type, dims = struct.unpack('>HBB', f.read(4))
-            
-            # Читаем размеры для каждого измерения (каждый размер - это 4-байтовое число '>I')
             shape = tuple(struct.unpack('>I', f.read(4))[0] for _ in range(dims))
-            
-            # Читаем оставшиеся данные как одномерный массив (тип 0x08 - это беззнаковый байт uint8)
             data = np.frombuffer(f.read(), dtype=np.uint8)
-            
-            # Превращаем одномерный массив в матрицу нужной формы
             return data.reshape(shape)
 
-    # 1. Считываем сырые данные из 4-х файлов
     X_train_raw = read_idx(train_images_path)
     Y_train = read_idx(train_labels_path)
     X_test_raw = read_idx(test_images_path)
     Y_test = read_idx(test_labels_path)
 
-    # 2. Подготавливаем матрицы для нейросети
-    # Исходная форма X_train_raw: (60000, 28, 28). 
-    # Нам нужно вытянуть картинки в столбцы, чтобы форма стала (784, 60000)
-    X_train = X_train_raw.reshape(X_train_raw.shape[0], -1).T
-    X_test = X_test_raw.reshape(X_test_raw.shape[0], -1).T
+    # Меняем форму (reshape) под стандарты сверточных сетей:
+    # m картинок, 1 цветовой канал, размер 28x28
+    X_train = X_train_raw.reshape(X_train_raw.shape[0], 1, 28, 28)
+    X_test = X_test_raw.reshape(X_test_raw.shape[0], 1, 28, 28)
 
-    # 3. Нормализуем данные (делаем значения пикселей от 0 до 1)
+    # Нормализуем данные (делаем значения пикселей от 0 до 1)
     X_train = X_train / 255.0
     X_test = X_test / 255.0
 
